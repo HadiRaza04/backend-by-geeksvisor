f1a9d4d44daf0b45a94b61339968e60447d4879a
hadi raza <hadiraza.edu@gmail.com>
2024-06-30 12:40:20 +0500

    added two more

--- class12 - backend/controllers/teacherController.js
+++ class12 - backend/controllers/teacherController.js
@@ -1,6 +1,5 @@
 const Teacher = require("../model/Teacher");
 
-
 const postTeacher = async (req, res) => {
     const { name, email, department } = req.body;
     try {
@@ -40,4 +39,31 @@ const getByIdTeacher = async (req, res) => {
     }
 }
 
-module.exports = { postTeacher, getTeachers, getByIdTeacher }
+const putTeacher = async (req, res) => {
+    const { name, email, department } = req.body;
+    try {
+        const id = req.params.id;
+        if( !name || !email || !department ){
+            return res.status(400).json({ error: "name or email or department are required" })
+        }
+        const updateTeacher = await Teacher.findOneAndUpdate( { _id: id }, { name, email, department }, { new: true } )
+        return res.status(200).json(updateTeacher)
+    } catch (error) {
+        return res.status(500).json({ error: "Server error", details: error.massage});
+    }
+}
+
+const deleteTeacher = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const teacher = await Teacher.findOneAndDelete( { _id: id } );
+        if(!teacher) {
+            return res.status(404).json({error: "Student not found"})
+        }
+        return res.status(200).json( { msg: "Student deleted." } )
+    } catch (error) {
+        return res.status(500).json({ error: "Server error", details: error.massage});
+    }
+}
+
+module.exports = { postTeacher, getTeachers, getByIdTeacher, putTeacher, deleteTeacher }
--- class12 - backend/router/teacher.js
+++ class12 - backend/router/teacher.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const Teacher = require('../model/Teacher');
-const { postTeacher, getTeachers, getByIdTeacher } = require('../controllers/teacherController');
+const { postTeacher, getTeachers, getByIdTeacher, putTeacher, deleteTeacher } = require('../controllers/teacherController');
 const teacherRouter = express.Router();
 
 teacherRouter.post('/', postTeacher)
@@ -9,30 +8,8 @@ teacherRouter.get('/', getTeachers)
 
 teacherRouter.get('/:id', getByIdTeacher)
 
-teacherRouter.put('/:id', async (req, res) => {
-    try {
-        const { name, email, department } = req.body;
-        const id = req.params.id;
-        if( !name || !email || !department ){
-            return res.status(400).json({ error: "name or email or department are required" })
-        }
-        const updateTeacher = await Teacher.findOneAndUpdate( { _id: id }, { name, email, department }, { new: true } )
-        return res.status(200).json(updateTeacher)
-    } catch (error) {
-        return res.status(500).json({ error: "Server error", details: error.massage});
-    }
-})
-teacherRouter.delete('/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const teacher = await Teacher.findOneAndDelete( { _id: id } );
-        if(!teacher) {
-            return res.status(404).json({error: "Student not found"})
-        }
-        return res.status(200).json( { msg: "Student deleted." } )
-    } catch (error) {
-        return res.status(500).json({ error: "Server error", details: error.massage});
-    }
-})
+teacherRouter.put('/:id', putTeacher)
+
+teacherRouter.delete('/:id', deleteTeacher)
 
 module.exports = teacherRouter
